38d803267b2d0e5cb4ee2c1bbd3ea2f91af3f00b
EC2 Default User <ec2-user@ip-172-31-31-247.us-east-2.compute.internal>
2020-04-26 02:53:04 +0000

    login works

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
 /* App Configuration */
 var express = require('express');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var mysql = require('mysql');
 var app = express();
+var passport = require('passport');
+var flash = require('express-flash');
+var session = require('express-session');
+
+
+
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(methodOverride('_method'));
 app.set('view engine', 'ejs');
+app.use(flash());
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 
 /* Configure MySQL DBMS */
 const connection = mysql.createConnection({
@@ -32,7 +52,7 @@ app.get('/', function(req, res){
 });
 
 
-//bookByTitle route
+/* The handler for the /bookByTitle route */
 app.get('/title', function(req, res){
     console.log(req.query.title);
      var stmt = 'select * from FP_books where title=\'' 
@@ -53,7 +73,7 @@ app.get('/title', function(req, res){
     });
 });
 
-//bookByYear route
+/* The handler for the /bookByYear route */
 app.get('/year', function(req, res){
     console.log(req.query.title);
      var stmt = 'select * from FP_books where year=\'' 
@@ -75,10 +95,10 @@ app.get('/year', function(req, res){
 });
 
 
-// author routes
+/* The handler for the /author route */
 app.get('/author', function(req, res){
     console.log(req.query.firstname, req.query.lastname);
-     var stmt = 'select * from FP_author where firstName=\'' 
+    var stmt = 'select * from FP_author where firstName=\'' 
                 + req.query.firstname + '\' and lastName=\'' 
                 + req.query.lastname + '\';';
     console.log(stmt);
@@ -103,10 +123,6 @@ app.get('/author/:aid', function(req, res){
                'where FP_books.authorId=FP_author.authorId ' + 
                'and FP_books.authorId=' + req.params.aid + ';'
     connection.query(stmt, function(error, results){
-        // if(error) throw error;
-        // var name = results[0].firstName + ' ' + results[0].lastName;
-        // res.render('booksByAuthor', {name: name, books: results});     
-        
         if(error){
             throw error;
         } else if(results.length){      //books by author is in db
@@ -125,20 +141,55 @@ app.get('/checkout/:aid', function(req, res){
                'where FP_books.authorId=FP_author.authorId ' + 
                'and FP_books.bookId=' + req.params.aid + ';'
     connection.query(stmt, function(error, results){
-        // if(error) throw error;
-        // var name = results[0].firstName + ' ' + results[0].lastName;
-        // res.render('booksByAuthor', {name: name, books: results});     
-        
         if(error){
             throw error;
-        } else if(results.length){      //books by author is in db
+        } else if(results.length){ 
             var name = results[0].firstName + ' ' + results[0].lastName;
             res.render('checkout', {name: name, books: results});     
-        } else {                        //books by author is not in db - do this as a pop up later
+        } else {                        
             console.log("No books by author found");
             res.render("error");
         }
     });
+});
+
+/* The handlers for the login routes */
+app.get('/login', function(req, res){
+  res.render('login');
+});
+
+app.post('/login', function(req, res){
+
+     var stmt = 'select * from FP_books, FP_user where userName=\'' 
+                + req.body.username + '\' and password=\'' 
+                + req.body.password + '\';';
+    console.log(stmt);
+    var user = null;
+    var books = null;
+    connection.query(stmt, function(error, results){
+        if(error){
+            throw error;
+        } else if(results.length){      //user is in db
+            // console.log(results);
+            user = results[results.length - 1].userName;
+            books = results; 
+            console.log("USER: " + user);
+            console.log("BOOKS: " + books);
+            res.render('home',{user: user, books: books});
+        } else {                        //user is not in db - do this as a pop up later
+            console.log("User not found");
+            res.render("error");
+        }
+    });
+});
+
+/* The handlers for the Register routes */
+app.get('/register', function(req, res){
+  res.render('register');
+});
+
+app.post('/register', function(req, res){
+  console.log('post register');
 });
 
 
